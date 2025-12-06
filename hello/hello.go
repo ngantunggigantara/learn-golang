df34c2d37131c0cf2791eff1b7d654cc283a94c9
gigantara.ngantung <gigantara.ngantung@buanafinance.co.id>
2025-12-06 12:38:19 +0700

    test

---
+++ hello/hello.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+//declare variable
+//var name string
+var a int
+var b int
+
+func main() {
+
+	a = 10
+	b = 5
+
+	fmt.Println(a + b)
+
+	// fmt.Println("Masukkan nama:")
+	// fmt.Scanln(&name)
+	// fmt.Printf("Hello %s, welcome to Go!", name)
+
+}
